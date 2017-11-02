db7b43fb68d48630cca608013a9580504b2c9293
Joseph <joekimga@gmail.com>
2017-11-02 07:51:13 -0400

    WIP: add more product to list

--- bamazon.sql
+++ bamazon.sql
@@ -15,14 +15,14 @@ CREATE TABLE products (
 
 SELECT * FROM products;
 
-INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("MINERAL B Round Carbon Steel Fry Pan 12.5-Inch", "Home & Kitchen", 60.00 , 8 );
+INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("MINERAL B Round Carbon Steel Fry Pan 12.5-Inch", "Home & Kitchen", 60.00 , 8);
 INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("Searzall Torch Attachment, Small, Stainless", "Home & Kitchen", 74.23, 0);
-INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("ChefSteps Joule Sous Vide, 1100 Watts, All White","Home & Kitchen", 179, 5);
+INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("ChefSteps Joule Sous Vide, 1100 Watts, All White","Home & Kitchen", 179.00, 5);
 
--- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
--- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
--- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
--- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("De Buyer Neoprene Cover Grip Protector Strip for Steel Pan Handles - 7.87 Inches Long","Home & Kitchen", 19.42, 25);
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("Extreme Matters Heavy Duty Pan Organizer - Bottom Tier 1 Inch Taller for Larger Pans - No Assembly Required - Black","Home & Kitchen", 24.97, 1);
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("The Ringer - The Original Stainless Steel Cast Iron Cleaner, Patented XL 8x6 inch Design","Home & Kitchen", 14.99, 1);
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("FlexiSpot Standing Desk - 47" wide platform Stand Up Desk Riser with Quick Release Keyboard Tray (L-Size-Black)","Office Products", 349.99, 1);
 -- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
 -- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
 -- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
