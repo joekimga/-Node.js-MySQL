41196eff58b725b3de4f476ec6cb24f77dcff77a
Joseph <joekimga@gmail.com>
2017-11-01 22:02:12 -0400

    WIP: with tutor

--- bamazon.sql
+++ bamazon.sql
@@ -4,26 +4,27 @@ CREATE database bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,
   price VARCHAR(100) NOT NULL,
   stock_quantity INT NOT NULL,
 
-  PRIMARY KEY (item_id)
+  PRIMARY KEY (default)
 );
 
 SELECT * FROM products;
 
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("MINERAL B Round Carbon Steel Fry Pan 12.5-Inch", "Home & Kitchen", 60.00 , 8 );
+INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("Searzall Torch Attachment, Small, Stainless", "Home & Kitchen", 74.23, 0);
+INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES ("ChefSteps Joule Sous Vide, 1100 Watts, All White","Home & Kitchen", 179, 5);
+
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
+-- INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("","", , );
 
 SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
-var prompt = require("prompt");
+//var prompt = require("prompt");
+var inquirer = require("inquirer");
+
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -14,7 +16,50 @@ connection.connect(function(error) {
 		console.log("Error connecting to Database");
 		return;
 	}
-	console.log("Connection Established");
+	console.log("Connection Established...Welcome to Bamazon");
+	startBamazon();
 
-	
-})
+/*
+	var schema = {
+		properties: {
+			ID: {
+			message: 	
+			}
+		}
+	}
+*/
+
+
+
+
+
+});
+
+//start cli table bamazon/app
+function startBamazon() {
+
+//query database to select all products
+var query = "querry" 
+
+
+// npm.mysql
+// connection.query('querry 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+
+
+
+
+//add all products to table
+
+//display table with cli table
+ 
+
+
+
+//inquirer to prompt user what to do
+
+
+}
